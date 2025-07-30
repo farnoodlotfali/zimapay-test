737c23cf335ca9627ea6069376035088069f45df
farnoodlotfali <farnoodlotfali@gmail.com>
2025-07-30 22:59:30 +0330

    fix image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Box } from "@mui/material";
 import Container from "@mui/material/Container";
 
 import PanelLayout from "../layouts/panel";
@@ -6,25 +7,28 @@ import PanelLayout from "../layouts/panel";
 export default function Home() {
   return (
     <PanelLayout>
-      <Container
-        maxWidth="lg"
-        sx={{
-          mt: 20,
-        }}
-      >
-        <Image
-          src="/ZimaLogo.svg"
-          sizes="100vw"
-          style={{
-            width: "100%",
-            height: "auto",
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            maxWidth: 400,
+            mx: "auto",
+            pt: 20,
           }}
-          width={258}
-          height={104}
-          alt="CashInRevenue"
-          loading="eager"
-          priority
-        />
+        >
+          <Image
+            src="/ZimaLogo.svg"
+            sizes="100vw"
+            style={{
+              width: "100%",
+              height: "auto",
+            }}
+            width={258}
+            height={104}
+            alt="CashInRevenue"
+            loading="eager"
+            priority
+          />
+        </Box>
       </Container>
     </PanelLayout>
   );
